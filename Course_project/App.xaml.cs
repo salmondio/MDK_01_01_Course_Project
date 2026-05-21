11d4aa3db660fbc5992e5e13232342317526de76
salmondio <sergej.serzanin1@gmail.com>
2026-05-21 23:03:48 +0500

    Initial commit

---
+++ Course_project/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Course_project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
